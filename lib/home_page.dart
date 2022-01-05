97557d946c4dceb904c6fced18732b8ddac5f48c
justin <justinkjoseph200@gmail.com>
2022-01-05 08:54:16 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var days = 30;
+    var str = "Justin";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Learning Flutter'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Welcome to $days of Flutter by - $str'),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
