cc715b10054f26d0f987a98d432e387d16f84afd
justin <justinkjoseph200@gmail.com>
2022-01-13 16:42:39 +0530

    Day6 Completed

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = '';
+  bool changedState = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -19,7 +26,7 @@ class LoginPage extends StatelessWidget {
                 height: 20,
               ),
               Text(
-                "Login Page",
+                "Welcome $name",
                 style: TextStyle(
                   fontSize: 22,
                   fontWeight: FontWeight.bold,
@@ -38,6 +45,12 @@ class LoginPage extends StatelessWidget {
                         labelText: "Username",
                         hintText: "Enter you Username ",
                       ),
+                      onChanged: (value) {
+                        //onChanged returns a value
+                        name = value; //that will be given to name
+                        //after this we need to rebuild whole project
+                        setState(() {});
+                      },
                     ),
                     TextFormField(
                       obscureText: true,
@@ -49,7 +62,7 @@ class LoginPage extends StatelessWidget {
                     SizedBox(
                       height: 40,
                     ),
-                    ElevatedButton(
+                    /*ElevatedButton(
                       child: Text("Login"),
                       style: TextButton.styleFrom(
                         minimumSize: Size(100, 30),
@@ -61,7 +74,47 @@ class LoginPage extends StatelessWidget {
                             MyRoutes
                                 .projectRoute); //but prefered one is this one
                       },
-                    )
+                    )*/
+                    //Instead of Elevated button animated button (Simple Animations)
+                    //Container() - an empty box
+                    //how to make any widget clickable
+                    //  -> gesturedetector(no feedback) and -> inkwell(gives feedback ie, like ripple effect )
+                    InkWell(
+                      onTap: () async {
+                        //Navigator.pushNamed(context, MyRoutes.projectRoute);
+                        setState(() {
+                          changedState = true;
+                        });
+                        await Future.delayed(Duration(
+                            milliseconds: 600)); //wait for 600 milliseconds
+                        Navigator.pushNamed(context, MyRoutes.projectRoute);
+                      },
+                      child: AnimatedContainer(
+                        duration: Duration(milliseconds: 600),
+                        width: changedState ? 50 : 150,
+                        height: 50,
+                        alignment: Alignment.center,
+                        child: changedState
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                        //Decorating the container
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            borderRadius: changedState
+                                ? BorderRadius.circular(50)
+                                : BorderRadius.circular(10)),
+                      ),
+                    ),
                   ],
                 ),
               ),
